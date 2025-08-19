9e84fe6de8dd51514906a492374314d340a7942c
inigosiswanto <inigo.siswanto12@gmail.com>
2025-08-19 20:07:15 +0700

    Added Main Menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Play Button
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Options()
+    {
+        Debug.Log("Option Button Clicked");
+    }
+
+    //Quit Button
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
