ad4d44aabc69a5ac30acf88ab2dfa0ed6f796a0c
inigosiswanto <inigo.siswanto12@gmail.com>
2025-08-30 01:24:50 +0700

    Made Volume Settings Work

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
 
     [Header("UI Panels")]
     public CanvasGroup optionsCanvas;
 
     private bool optionsOpen = false;
 
+
+    private void start()
+    {
+        LoadVolume();
+    }
+
+    // ======== Buttons =========
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -25,12 +39,39 @@ public class MainMenu : MonoBehaviour
         optionsCanvas.blocksRaycasts = optionsOpen;
 
         Debug.Log("Options Panel " + (optionsOpen ? "Opened" : "Closed"));
-        
+
     }
 
-    //Quit Button
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+
+    //=========Volume=========
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
