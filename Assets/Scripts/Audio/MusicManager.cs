d0aa62d7e709f9daa9ba263067437a53ef59cad1
inigosiswanto <inigo.siswanto12@gmail.com>
2025-08-29 23:52:28 +0700

    Updated MusicManager Script

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MusicManager Instance;
+
+    [SerializeField]
+    private MusicLibrary musicLibrary;
+    [SerializeField]
+    private AudioSource musicSource;
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayMusic(string trackName, float fadeDuration = 0.5f)
     {
-        
+        StartCoroutine(AnimateMusicCrossfade(musicLibrary.GetClipFromName(trackName), fadeDuration));
+    }
+
+    IEnumerator AnimateMusicCrossfade(AudioClip nextTrack, float fadeDuration = 0.5f)
+    {
+        float percent = 0;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * 1 / fadeDuration;
+            musicSource.volume = Mathf.Lerp(1f, 0, percent);
+            yield return null;
+        }
+
+        musicSource.clip = nextTrack;
+        musicSource.Play();
+
+        percent = 0;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * 1 / fadeDuration;
+            musicSource.volume = Mathf.Lerp(0, 1f, percent);
+            yield return null;
+        }
     }
 }
