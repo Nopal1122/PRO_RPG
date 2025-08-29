228a54b07729b0ab2bd168e0bf68f72b5b92835f
inigosiswanto <inigo.siswanto12@gmail.com>
2025-08-29 20:55:42 +0700

    Added New PlayerPos to ChangeScene

--- Assets/Scripts/SceneChanger/SceneChanger.cs
+++ Assets/Scripts/SceneChanger/SceneChanger.cs
@@ -9,12 +9,15 @@ public class SceneChanger : MonoBehaviour
     public string SceneToLoad;
     public Animator fadeAnim;
     public float fadeTime = 1f;
+    public Vector2 newPlayerPosition;
+    private Transform player;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            player = collision.transform;
             fadeAnim.Play("FadeToBlack");
             StartCoroutine(DelayFade());
         }
@@ -23,6 +26,7 @@ public class SceneChanger : MonoBehaviour
     IEnumerator DelayFade()
     {
         yield return new WaitForSeconds(fadeTime);
+        player.position = newPlayerPosition;
         SceneManager.LoadScene(SceneToLoad);
     }
 }
